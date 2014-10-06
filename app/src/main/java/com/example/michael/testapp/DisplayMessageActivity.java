421af9983c41461f62f24b9d2aac8d97203c9d4d
ChaosBelcebub <mich3ael@web.de>
2014-10-06 23:26:43 +0200

    delete text view, add grid. main's text insert useless

--- app/src/main/java/com/example/michael/testapp/DisplayMessageActivity.java
+++ app/src/main/java/com/example/michael/testapp/DisplayMessageActivity.java
@@ -2,48 +2,60 @@ package com.example.michael.testapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
+import android.graphics.Color;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.content.Intent;
-import android.widget.TextView;
-
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class DisplayMessageActivity extends Activity {
+
+    RelativeLayout rl;
+    GridView grid;
+    List<String> list;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
 
-        // Get the message from the intent
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(FirstActivity.EXTRA_MESSAGE);
+        rl=(RelativeLayout) findViewById(R.id.rl);
+        grid =new GridView(DisplayMessageActivity.this);
+        list=new ArrayList<String> ();
 
-        // Create the text view
-        TextView textView = new TextView(this);
-        textView.setTextSize(40);
-        textView.setText(message);
+        list.add("Dynamic 1");
+        list.add("Dynamic 2");
+        list.add("Dynamic 3");
+        list.add("Dynamic 4");
+        list.add("Dynamic 5");
+        list.add("Dynamic 6");
+        list.add("Dynamic 7");
+        list.add("Dynamic 8");
+        list.add("Dynamic 9");
 
-        // Set the text view as the activity layout
-        setContentView(textView);
-    }
+        ArrayAdapter<String> adp=new ArrayAdapter<String> (this,
+                android.R.layout.simple_dropdown_item_1line,list);
+        grid.setNumColumns(3);
+        grid.setBackgroundColor(Color.BLACK);
+        grid.setAdapter(adp);
+        rl.addView(grid);
 
+        grid.setOnItemClickListener(new OnItemClickListener() {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.display_message, menu);
-        return true;
-    }
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+                                    long arg3) {
+                // TODO Auto-generated method stub
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+                Toast.makeText(getBaseContext(), list.get(arg2),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
